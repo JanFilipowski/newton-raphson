87125c9cc607788c50dabd6540772d0d86e15407
Jan Filipowski <kontakt@panjan.com.pl>
2025-04-08 21:02:35 +0000

    Initial commit

---
+++ EAN_MAIN.pas
@@ -0,0 +1,247 @@
+﻿unit EAN_MAIN;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  Vcl.StdCtrls, System.StrUtils, Vcl.ExtCtrls, Vcl.Buttons, EAN_RESULTS, EAN_DESCRIPTION;
+
+type
+  TFormMain = class(TForm)
+    grpArithmetic: TGroupBox;
+    rbArithmeticStd: TRadioButton;
+    rbArithmeticInterval: TRadioButton;
+    grpData: TGroupBox;
+    rbDataStd: TRadioButton;
+    rbDataInterval: TRadioButton;
+    lblX: TLabel;
+    editX: TEdit;
+    editXA: TEdit;
+    editXB: TEdit;
+    grpAdvanced: TGroupBox;
+    lblMit: TLabel;
+    editMit: TEdit;
+    lblEps: TLabel;
+    editEps: TEdit;
+    btnSolve: TButton;
+    BitBtnClose: TBitBtn;
+    Label1: TLabel;
+    BitBtnInfo: TBitBtn;
+    OpenDialog1: TOpenDialog;
+    Button1: TButton;
+    btnLoad: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure RadioClick(Sender: TObject);
+    procedure EditFloatExit(Sender: TObject);
+    procedure EditIntExit(Sender: TObject);
+    procedure btnSolveClick(Sender: TObject);
+    procedure BitBtnInfoClick(Sender: TObject);
+    procedure btnLoadClick(Sender: TObject);
+  private
+    procedure UpdateVisibility;
+    function ValidateFloat(Edit: TEdit) : Boolean;
+    procedure ValidateInt(Edit: TEdit);
+  public
+    { Public declarations }
+  end;
+
+var
+  FormMain: TFormMain;
+  isIntervalArithmetic, isIntervalData: Boolean;
+  x, xA, xB, dllLoc : string;
+  eps: Extended;
+  mit, chosenEquation: Integer;
+
+implementation
+
+{$R *.dfm}
+
+{ **************************************************************************** }
+{                       Procedury pomocnicze i zdarzenia                       }
+{ **************************************************************************** }
+
+procedure TFormMain.FormCreate(Sender: TObject);
+begin
+  rbArithmeticStd.Checked := True;
+  rbDataStd.Checked := True;
+
+  UpdateVisibility;
+end;
+
+
+
+procedure TFormMain.RadioClick(Sender: TObject);
+begin
+  if rbArithmeticStd.Checked then
+  begin
+    rbDataInterval.Enabled := False;
+    rbDataStd.Checked := True;
+  end
+  else
+  begin
+    rbDataInterval.Enabled := True;
+  end;
+
+  UpdateVisibility;
+end;
+
+procedure TFormMain.UpdateVisibility;
+begin
+  if rbDataStd.Checked then
+  begin
+    lblX.Caption := 'Podaj wartość x';
+    editX.Visible := True;
+    editXA.Visible := False;
+    editXB.Visible := False;
+  end
+  else
+  begin
+    lblX.Caption := 'Podaj wartość końców przedziału x.a x.b';
+    editX.Visible := False;
+    editXA.Visible := True;
+    editXB.Visible := True;
+  end;
+
+
+end;
+
+function TFormMain.ValidateFloat(Edit: TEdit) : Boolean;
+var
+  Value: Extended;
+  TextVal: string;
+  fs: TFormatSettings;
+begin
+  TextVal := Trim(Edit.Text);
+
+  if TextVal = '' then
+  begin
+    ShowMessage('Pole "' + Edit.Name + '" jest puste. Wpisz liczbę zmiennoprzecinkową.');
+    Edit.SetFocus;
+    Exit;
+  end
+  else
+  begin
+
+    TextVal := StringReplace(TextVal, ',', '.', [rfReplaceAll]);
+    fs := TFormatSettings.Create;
+    fs.DecimalSeparator := '.';
+
+    if TryStrToFloat(TextVal, Value, fs) then
+      Result := True
+    else begin
+      ShowMessage('Błędny format liczby zmiennoprzecinkowej: "' + Edit.Text + '"');
+      Edit.SetFocus;
+      Result := False;
+      Exit;
+    end;
+  end;
+end;
+
+procedure TFormMain.ValidateInt(Edit: TEdit);
+var
+  Value: Integer;
+  TextVal: string;
+begin
+  TextVal := Trim(Edit.Text);
+
+  if TextVal = '' then
+  begin
+    ShowMessage('Pole "' + Edit.Name + '" jest puste. Wpisz liczbę całkowitą.');
+    Edit.SetFocus;
+    //raise Exception.Create('Pole puste');
+  end;
+
+  if not TryStrToInt(TextVal, Value) then
+  begin  
+    ShowMessage('Błędny format liczby całkowitej: "' + Edit.Text + '"');
+    Edit.SetFocus;
+    //raise Exception.Create('Błędny format integer');
+  end else if StrToInt(TextVal) <= 0 then
+  begin
+    ShowMessage('Liczba iteracji musi być większa od zera');
+    Edit.SetFocus;
+  end;
+           
+end;
+
+procedure TFormMain.EditFloatExit(Sender: TObject);
+begin
+  ValidateFloat(TEdit(Sender));
+end;
+
+procedure TFormMain.EditIntExit(Sender: TObject);
+begin
+  ValidateInt(TEdit(Sender));
+end;
+
+
+procedure TFormMain.BitBtnInfoClick(Sender: TObject);
+var
+  frmDesc: TFormDescription;
+begin
+  frmDesc := TFormDescription.Create(Self);
+  try
+    frmDesc.ShowModal;
+  finally
+    frmDesc.Free;
+  end;
+end;
+
+
+procedure TFormMain.btnLoadClick(Sender: TObject);
+begin
+  if OpenDialog1.Execute then
+    dllLoc := OpenDialog1.FileName;
+end;
+procedure TFormMain.btnSolveClick(Sender: TObject);
+var
+  frmResults: TfrmEANResults;
+begin
+  FormatSettings.DecimalSeparator := '.';
+
+  isIntervalArithmetic := rbArithmeticInterval.Checked;
+  isIntervalData := rbDataInterval.Checked;
+
+  if not isIntervalData then
+  begin
+    if ValidateFloat(editX) then
+    begin
+      xA := StringReplace(editX.Text, ',', '.', [rfReplaceAll]);
+      xB := xA;
+    end
+    else Exit;
+  end
+  else
+  begin
+    if (ValidateFloat(editXA) and ValidateFloat(editXB)) then
+    begin
+      xA := StringReplace(editXA.Text, ',', '.', [rfReplaceAll]);
+      xB := StringReplace(editXB.Text, ',', '.', [rfReplaceAll]);
+      if StrToFloat(xA) > StrToFloat(xB) then
+      begin
+        ShowMessage('Prawy koniec przedziału jest mniejszy niż lewy koniec przedziału');
+        Exit;
+      end;
+    end
+    else Exit;
+  end;
+
+  mit := StrToInt(editMit.Text);
+  eps := StrToFloat(StringReplace(editEps.Text, ',', '.', [rfReplaceAll]));
+
+
+  // Utworzenie i wyświetlenie okna wyników
+  frmResults := TfrmEANResults.Create(Self);
+  try
+    frmResults.CalcResults(xA, xB, isIntervalArithmetic, isIntervalData, mit, eps, dllLoc);
+    frmResults.ShowModal;
+  finally
+    frmResults.Free;
+  end;
+
+  // Właściwe obliczenia możesz umieścić tutaj...
+end;
+
+
+end.
--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# newton-raphson
-Ten projekt jest dedykowany do znajdowania przybliżonego pierwiastka równania nieliniowego f(x)=0 z wykorzystaniem metody Newtona-Raphsona
+# Program do znajdowania rozwiązania równania nieliniowego metodą Newtona-Raphsona
+
+## Opis
+Ten projekt jest dedykowany do znajdowania przybliżonego pierwiastka równania nieliniowego 
+$
+f(x) = 0.
+$
+Algorytm realizuje iteracyjną metodę Newtona-Raphsona drugiego rzędu, który wykorzystuje zarówno pierwszą, jak i drugą pochodną funkcji do wyznaczenia kolejnych przybliżeń pierwiastka.
+
+## Algorytm
+Pierwiastek równania wyznacza się przy pomocy następującej iteracji:
+$
+x_{i+1} = x_i - \frac{f'(x_i) \pm \sqrt{[f''(x_i)]^2 - 2\,f'(x_i)\,f''(x_i)}}{f''(x_i)}, \quad i = 0,1,\ldots
+$
+gdzie $x_0$ jest wartością początkową podaną przez użytkownika. Proces iteracyjny zatrzymuje się, gdy:
+$
+\frac{\lvert x_{i+1} - x_i\rvert}{\max(\lvert x_{i+1}\rvert, \lvert x_{i}\rvert)} < \varepsilon, \quad (x_{i+1} \neq 0 \text{ lub } x_i \neq 0)
+$
+lub gdy przy kolejnych iteracjach nie zmienia się już wartość $x$.
+
+## Status Algorytmu
+Poprawność wykonania algorytmu jest sygnalizowana przez zmienną `st` (status), która przyjmuje jedną z następujących wartości:
+$
+\texttt{st} = 
+\begin{cases}
+1, & \text{jeżeli } i = 1,\\[1mm]
+2, & \text{gdy podczas obliczeń } f'(x) = 0 \text{ dla pewnej wartości } x,\\[1mm]
+3, & \text{jeżeli w } n_I \text{ krokach iteracyjnych nie osiągnięto podanej dokładności},\\[1mm]
+4, & \text{gdy } f''(x) - 2\,f'(x)\,f''(x) < 0 \text{ dla pewnej wartości } x,\\[1mm]
+5, & \text{w przeciwnym przypadku}.
+\end{cases}
+$
+**Uwaga:** Jeśli $\texttt{st} = 1, 2 \text{ lub } 4$, wartość funkcji nie jest obliczana, a w przypadku $\texttt{st} = 3$ ostatnie przybliżenie pierwiastka jest zwracane.
+
+## Kluczowe Funkcjonalności
+- **Metoda Newtona-Raphsona drugiego rzędu**  
+  Wykorzystanie zarówno pierwszej, jak i drugiej pochodnej funkcji w celu szybszej i bardziej dokładnej konwergencji.
+- **Użycie arytmetyki zmiennopozycyjnej lub przedziałowej**  
+  Użytkownik ma możliwość wyboru użycia arytmetyki zmiennopozycyjnej, jak i przedziałowej (wykorzystując bibliotekę IntervalArithmetic32and64 Copyright © 1998-2025 by Andrzej Marciniak)
+- **Dynamiczne ładowanie bibliotek DLL**  
+  Funkcje definiowane są w zewnętrznych bibliotekach DLL (np. TestPoly.dll, TestTrig.dll). 
+  Aplikacja główna ładuje wybraną bibliotekę w trakcie działania, co zwiększa elastyczność rozwiązania.
+
+<!--
+## Instalacja i Uruchomienie
+1. **Klonowanie repozytorium:**  
+   Skopiuj repozytorium z GitHuba:
+   https://github.com/JanFilipowski/newton-raphson
+
+2. **Budowanie DLL:**  
+   - Otwórz projekt DLL (np. TestPoly i TestTrig) w Delphi.  
+   - Upewnij się, że wszystkie jednostki (w tym IntervalArithmetic32and64) są dostępne i poprawnie skonfigurowane.  
+   - Skonfiguruj i zbuduj projekty, aby wygenerować odpowiednie pliki .dll.
+
+3. **Budowanie aplikacji głównej:**  
+   - Otwórz projekt aplikacji głównej (EAN_MAIN) w Delphi.  
+   - Upewnij się, że ścieżki do wygenerowanych DLL są poprawnie ustawione.  
+   - Skonfiguruj projekt i zbuduj aplikację.
+
+4. **Uruchomienie:**  
+   Po uruchomieniu aplikacji głównej wybierz odpowiednią bibliotekę DLL poprzez okno dialogowe (TOpenDialog).  
+   Następnie wprowadź wymagane parametry (np. $x_0$, dokładność $\varepsilon$, liczbę iteracji) i uruchom algorytm.
+
+-->
+## Kontakt i Wsparcie
+- URL projektu: https://github.com/yourusername/program-do-znajdowania-rozwiazania-rownania-newtona-raphsona
+- W razie pytań lub problemów, zgłoś je poprzez Issues na GitHubie.
+
+## Licencja
+Ten projekt jest udostępniony na licencji GNU GPL. Szczegóły zawarte są w pliku LICENSE.
